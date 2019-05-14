34e3ef9564bf336199568e8a0a7b74bb0469a59d
ujwal.tale <anup@inevitablesol.com>
2019-05-14 16:32:34 +0530

    adding node modules

---
+++ build/precache-manifest.7bd7c5ec1915ea6c06326d8f69d6e756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d3119f96c4ccb381b282",
+    "url": "/static/js/main.d3119f96.chunk.js"
+  },
+  {
+    "revision": "9a11f47b9de7964e944f",
+    "url": "/static/js/1.9a11f47b.chunk.js"
+  },
+  {
+    "revision": "d3119f96c4ccb381b282",
+    "url": "/static/css/main.230ecc39.chunk.css"
+  },
+  {
+    "revision": "edd05726d99b6c6ed19990fb69c971be",
+    "url": "/index.html"
+  }
+];
